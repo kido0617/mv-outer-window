c8266c7ceca61044179010bd5aba5e8ffd465278
kido0617 <enokido0617@gmail.com>
2017-12-06 19:11:17 +0900

    update

--- js/plugins.js
+++ js/plugins.js
@@ -3,5 +3,5 @@
 var $plugins =
 [
 {"name":"OuterWindow","status":true,"description":"画面外演出をするためにサブウィンドウを表示するプラグイン","parameters":{"windowPath":"subWindow"}},
-{"name":"Debug","status":true,"description":"タイトル画面ぶっ飛ばして、デベロッパーツールをデフォで出すやつ","parameters":{"needDeveloperTool":"true"}}
+{"name":"Debug","status":true,"description":"タイトル画面ぶっ飛ばして、デベロッパーツールをデフォで出すやつ","parameters":{"needDeveloperTool":"false"}}
 ];
